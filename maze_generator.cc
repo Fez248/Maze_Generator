6729bade33b046d9c28ac9836b0b89be31723f89
Ferran <118133173+Fez248@users.noreply.github.com>
2023-11-14 23:21:07 +0100

    Add files via upload

---
+++ maze_generator.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+typedef vector<char> Row;
+typedef vector<Row> Maze;
+
+struct position
+{
+    int x;
+    int y;
+};
+
+void gen_cami(Maze& map, const int sx, const int sy, position& start, position&exit)
+{
+    int lx = exit.x - start.x;
+    int ly = exit.y - start.y;
+    while(lx != 0 or ly != 0)
+    {
+        map[start.x][start.y] = '.';
+
+    }
+}
+
+void gen_maze(Maze& map, const int sx, const int sy, position& start, position& exit)
+{
+    srand(time(NULL));
+}
+
+int main ()
+{
+
+}
