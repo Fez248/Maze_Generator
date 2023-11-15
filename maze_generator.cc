7694368dd601552724b449069e2b5ba1f0a3c815
Fez248 <ferran.beneitez@estudiantat.upc.edu>
2023-11-15 22:48:34 +0100

    Revert "Avances"
    This reverts commit 8a07e6fb5c1739b1777ca151d19d4f40c2cc329c.

--- maze_generator.cc
+++ maze_generator.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <stdlib.h>
 #include <time.h>
 using namespace std;
@@ -7,50 +6,11 @@ using namespace std;
 typedef vector<char> Row;
 typedef vector<Row> Maze;
 
-typedef vector<bool> Row2;
-typedef vector<Row2> Maze2;
-
 struct position
 {
     int x;
     int y;
 };
-
-bool in_range(const int sx, const int sy, int x, int y)
-{
-    return x < sx and x > -1 and y < sy and y > -1;
-}
-
-bool prision(const Maze2& visited, const int sx, const int sy, int i, int j)
-{
-    bool r1, r2, r3, r4;
-    if (in_range(sx, sy, i + 1, j))
-    {
-        if (visited[i + 1][j]) r1 = true;
-        else r1 = false;
-    }
-    else r1 = true;
-    if (in_range(sx, sy, i - 1, j))
-    {
-        if (visited[i - 1][j]) r2 = true;
-        else r2 = false;
-    }
-    else r2 = true;
-    if (in_range(sx, sy, i, j + 1))
-    {
-        if (visited[i][j + 1]) r3 = true;
-        else r3 = false;
-    }
-    else r3 = true;
-    if (in_range(sx, sy, i, j - 1))
-    {
-        if (visited[i][j - 1]) r4 = true;
-        else r4 = false;
-    }
-    else r4 = true;
-
-    return 
-}
 
 void gen_cami(Maze& map, const int sx, const int sy, position& start, position&exit)
 {
@@ -70,30 +30,5 @@ void gen_maze(Maze& map, const int sx, const int sy, position& start, position& 
 
 int main ()
 {
-    int sx, sy;
-    sx = sy = 20;
-    int h = 0;
 
-    while (h < 20)
-    {
-        srand(time(NULL) + h);
-        Maze map(sx, Row(sy, 'X'));
-
-        position start, exit;
-
-        gen_cami(map, sx, sy, start, exit);
-
-        for (int i = 0; i < sx; ++i)
-        {
-            for (int j = 0; j < sy; ++j)
-            {
-                cout << map[i][j];
-            }
-            cout << endl;
-        }   
-
-        cout << endl;
-        cout << "____________________" << endl;
-        ++h;
-    }
 }
