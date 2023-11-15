5f152024bc7d84b14f3d9edaea2e5333475a3147
Fez248 <ferran.beneitez@estudiantat.upc.edu>
2023-11-16 00:03:25 +0100

    Weird infinite loop

--- maze_generator.cc
+++ maze_generator.cc
@@ -8,13 +8,12 @@ I forgor: https://docs.github.com/en/get-started/using-git/getting-changes-from-
 #include <vector>
 #include <stdlib.h>
 #include <time.h>
-using namespace std;
 
-typedef vector<char> Row;
-typedef vector<Row> Maze;
+typedef std::vector<char> Row;
+typedef std::vector<Row> Maze;
 
-typedef vector<bool> Row2;
-typedef vector<Row2> Maze2;
+typedef std::vector<bool> Row2;
+typedef std::vector<Row2> Maze2;
 
 struct position
 {
@@ -29,83 +28,89 @@ bool in_range(const int sx, const int sy, int x, int y)
 
 bool prision(const Maze2& visited, const int sx, const int sy, int i, int j)
 {
-    bool r1, r2, r3, r4;
-    if (in_range(sx, sy, i + 1, j))
-    {
-        if (visited[i + 1][j]) r1 = true;
-        else r1 = false;
-    }
-    else r1 = true;
-    if (in_range(sx, sy, i - 1, j))
-    {
-        if (visited[i - 1][j]) r2 = true;
-        else r2 = false;
-    }
-    else r2 = true;
-    if (in_range(sx, sy, i, j + 1))
-    {
-        if (visited[i][j + 1]) r3 = true;
-        else r3 = false;
-    }
-    else r3 = true;
-    if (in_range(sx, sy, i, j - 1))
-    {
-        if (visited[i][j - 1]) r4 = true;
-        else r4 = false;
-    }
-    else r4 = true;
+    int conected = 0;
+    if (in_range(sx, sy, i + 1, j) and visited[i + 1][j]) ++conected;
+    if (in_range(sx, sy, i - 1, j) and visited[i - 1][j]) ++conected;
+    if (in_range(sx, sy, i, j + 1) and visited[i][j + 1]) ++conected;
+    if (in_range(sx, sy, i, j - 1) and visited[i][j - 1]) ++conected;
+    return conected;
+}
+
+void reset(std::vector<bool>& tried)
+{
+    for (int i = 0; i < 4; ++i) tried[i] = false;
 }
 
 void gen_cami(Maze& map, const int sx, const int sy, position& start, position&exit)
 {
-    int length = rand() % (sx * sy / 2);
+    Maze2 visited(sx, Row2(sy, false));
+    int length = (sx * sy) / 2;
     int i, j;
+
     i = exit.x = sx - 1;
     j = exit.y = sy -1;
-    Maze2 visited(sx, Row2(sy, false));
     visited[i][j] = true;
-    pair<int, int> anterior = make_pair(i, j);
+
+    std::vector<bool> tried(4, false);
 
     while (length != 0)
     {
         int dir = rand() % 4;
+        while(tried[dir])
+        {
+            if (dir == 3) dir = 0;
+            else ++dir;
+        }
+
         switch(dir)
         {
             case 0:
-                if (in_range(sx, sy, i + 1, j) and !visited[i + 1][j])
+                if (in_range(sx, sy, i + 1, j) and !visited[i + 1][j] and 2 > prision(visited, sx, sy, i + 1, j))
                 {
+                    std::cout << "d" << std::endl;
                     map[i + 1][j] = '.';
                     visited[i + 1][j] = true;
                     --length;
                     ++i;
+                    reset(tried);
                 }
+                else tried[dir] = true;
                 break;
             case 1:
-                if (in_range(sx, sy, i - 1, j) and !visited[i -1][j])
+                if (in_range(sx, sy, i - 1, j) and !visited[i -1][j] and 2 > prision(visited, sx, sy, i - 1, j))
                 {
+                    std::cout << "i" << std::endl;
                     map[i - 1][j] = '.';
-                    visited[i -1][j] = true;
+                    visited[i - 1][j] = true;
                     --length;
                     --i;
+                    reset(tried);
                 }
+                else tried[dir] = true;
                 break;
             case 2:
-                if (in_range(sx, sy, i, j + 1) and !visited[i][j + 1])
+                if (in_range(sx, sy, i, j + 1) and !visited[i][j + 1] and 2 > prision(visited, sx, sy, i, j + 1))
                 {
+                    std::cout << "a" << std::endl;
                     map[i][j + 1] = '.';
                     visited[i][j + 1] = true;
                     --length;
                     ++j;
+                    reset(tried);
                 }
+                else tried[dir] = true;
                 break;
             case 3:
-                if (in_range(sx, sy, i, j - 1) and !visited[i][j - 1])
+                if (in_range(sx, sy, i, j - 1) and !visited[i][j - 1] and 2 > prision(visited, sx, sy, i, j - 1))
                 {
+                    std::cout << "ab" << std::endl;
                     map[i][j - 1] = '.';
                     visited[i][j -1] = true;
                     --length;
                     --j;
+                    reset(tried);
                 }
+                else tried[dir] = true;
                 break;
 
             default:
@@ -130,7 +135,7 @@ int main ()
     sx = sy = 20;
     int h = 0;
 
-    while (h < 20)
+    while (h < 1)
     {
         srand(time(NULL) + h);
         Maze map(sx, Row(sy, 'X'));
@@ -143,13 +148,13 @@ int main ()
         {
             for (int j = 0; j < sy; ++j)
             {
-                cout << map[i][j];
+                std::cout << map[i][j];
             }
-            cout << endl;
+            std::cout << std::endl;
         }   
 
-        cout << endl;
-        cout << "____________________" << endl;
+        std::cout << std::endl;
+        std::cout << "____________________" << std::endl;
         ++h;
     }
 }
