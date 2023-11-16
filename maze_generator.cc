ba243549f8c00d6d7e856a4ccf54d32ccc740d44
Fez248 <ferran.beneitez@estudiantat.upc.edu>
2023-11-16 11:02:57 +0100

    More readable

--- maze_generator.cc
+++ maze_generator.cc
@@ -2,6 +2,14 @@
 Creator: Ferran Benéitez Borrut
 Last Edit: 16/11/2023
 I forgor: https://docs.github.com/en/get-started/using-git/getting-changes-from-a-remote-repository
+
+Works for small mazes, with dimensions of 50 x 50 it starts to crete relative small paths due
+to the fact that it traps himself. Hovewer, the idea, is to run the same algorithm x times on the same map
+and take the largest path formed as the solution. All the other ones remaining will be dead ends that make the maze
+more difficult. The problem I have to face know is making the algorithm able to cross other paths perpendiculary,
+what I mean by that is making sure it doesn't creatre any area bigger than 1 x 1.
+
+Also the code still needs a lot of work.
 **/
 
 #include <iostream>
@@ -20,6 +28,18 @@ struct position
     int x;
     int y;
 };
+
+void directions(int& dir, int& stucked, const std::vector<bool>& tried)
+{
+    dir = rand() % 4;
+    while(tried[dir] and stucked < 4)
+    {
+        if (dir == 3) dir = 0;
+        else ++dir;
+        ++stucked;
+    }
+    if (stucked != 4) stucked = 0;
+}
 
 bool in_range(const int sx, const int sy, int x, int y)
 {
@@ -43,35 +63,25 @@ void reset(std::vector<bool>& tried)
 
 void gen_cami(Maze& map, const int sx, const int sy, position& start, position&exit)
 {
-    Maze2 visited(sx, Row2(sy, false));
-    int i, j, max;
+    std::vector<bool> tried(4, false);                                                  //Tried directions to know when is trapped
 
-    max = (sx * sy) / 4;
-    i = exit.x = sx - 1;
+    int i, j, max, stucked, dir;
+    max = (sx * sy) / 4;                                                                //Maz lenght that the pace can take
+    i = exit.x = sx - 1;                                                                //We start from the exit
     j = exit.y = sy -1;
-    map[i][j] = '.';
-    visited[i][j] = true;
-    int fuck = 0;
-    std::vector<bool> tried(4, false);
+    stucked = 0;
 
-    while (fuck != 4 and max != 0)
+    map[i][j] = '.';                                                                    //This coordinate with later get replaced by E but it's initialized as . for prision()
+
+    while (stucked != 4 and max != 0)
     {
-        int dir = rand() % 4;
-        while(tried[dir] and fuck < 4)
-        {
-            if (dir == 3) dir = 0;
-            else ++dir;
-            ++fuck;
-        }
-        if (fuck != 4) fuck = 0;
-
+        directions(dir, stucked, tried);
         switch(dir)
         {
             case 0:
-                if (in_range(sx, sy, i + 1, j) and !visited[i + 1][j] and !prision(map, sx, sy, i + 1, j)) //abajo
+                if (in_range(sx, sy, i + 1, j) and map[i + 1][j] != '.' and !prision(map, sx, sy, i + 1, j)) //abajo
                 {
                     map[i + 1][j] = '.';
-                    visited[i + 1][j] = true;
                     --max;
                     ++i;
                     reset(tried);
@@ -79,10 +89,9 @@ void gen_cami(Maze& map, const int sx, const int sy, position& start, position&e
                 else tried[dir] = true;
                 break;
             case 1:
-                if (in_range(sx, sy, i - 1, j) and !visited[i -1][j] and !prision(map, sx, sy, i - 1, j)) //arriba
+                if (in_range(sx, sy, i - 1, j) and map[i -1][j] != '.' and !prision(map, sx, sy, i - 1, j)) //arriba
                 {
                     map[i - 1][j] = '.';
-                    visited[i - 1][j] = true;
                     --max;
                     --i;
                     reset(tried);
@@ -90,10 +99,9 @@ void gen_cami(Maze& map, const int sx, const int sy, position& start, position&e
                 else tried[dir] = true;
                 break;
             case 2:
-                if (in_range(sx, sy, i, j + 1) and !visited[i][j + 1] and !prision(map, sx, sy, i, j + 1)) //derecha
+                if (in_range(sx, sy, i, j + 1) and map[i][j + 1] != '.' and !prision(map, sx, sy, i, j + 1)) //derecha
                 {;
                     map[i][j + 1] = '.';
-                    visited[i][j + 1] = true;
                     --max;
                     ++j;
                     reset(tried);
@@ -101,10 +109,9 @@ void gen_cami(Maze& map, const int sx, const int sy, position& start, position&e
                 else tried[dir] = true;
                 break;
             case 3:
-                if (in_range(sx, sy, i, j - 1) and !visited[i][j - 1] and !prision(map, sx, sy, i, j - 1)) //izquierda
+                if (in_range(sx, sy, i, j - 1) and map[i][j - 1] != '.' and !prision(map, sx, sy, i, j - 1)) //izquierda
                 {
                     map[i][j - 1] = '.';
-                    visited[i][j -1] = true;
                     --max;
                     --j;
                     reset(tried);
@@ -126,10 +133,10 @@ void gen_cami(Maze& map, const int sx, const int sy, position& start, position&e
 int main ()
 {
     int sx, sy;
-    sx = sy = 10;
+    sx = sy = 50;
     int h = 0;
 
-    while (h < 10)
+    while (h < 1)
     {
         srand(time(NULL) + h);
         Maze map(sx, Row(sy, 'X'));
